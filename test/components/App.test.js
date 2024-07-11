a568b026cbfc2ff9496792f612c6db147d3c948b
Paul Visciano <paulvisciano.dev@gmail.com>
2024-07-10 22:38:57 -0500

    Fixing path to App

--- test/components/App.test.js
+++ test/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../../src/client/components/App';
+import App from '../../src/App';
 
 test('Should render App component', () => {
   const wrapper = shallow(<App />);
